669b2c89b636cb8d4d956bc9d8d9bfca32501f55
solfra <f.soldano38@orange.fr>
2026-03-27 17:15:33 +0100

    Add docstring

--- Fits_tools/fits_preview.py
+++ Fits_tools/fits_preview.py
@@ -5,6 +5,17 @@ def fits_preview(file_name,ext=1,normlise=True):
     data = fits.open(file_name)
     
 def extract_header(file_name,ext=1,verbose=False):
+    """
+    Open a fits file and extract the header of a given extension
+
+    Input:
+    - file_name (str): file name to read
+    - ext (int or str): Name or number of the extension. Default is 1.
+    - verbose (bool): If true print the header. Default is False.
+
+    Output:
+    -header (dict): The header pf the extension.  
+    """
     header = fits.open(file_name)[ext].header
     if verbose:
         print(header)
