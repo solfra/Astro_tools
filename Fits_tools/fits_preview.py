d777dbb3e30b250c43b283871fc25b8b5b1488d2
solfra <f.soldano38@orange.fr>
2026-03-27 17:11:30 +0100

    Add header read

--- Fits_tools/fits_preview.py
+++ Fits_tools/fits_preview.py
@@ -4,5 +4,8 @@ import matplotlib.pyplot as plt
 def fits_preview(file_name,ext=1,normlise=True):
     data = fits.open(file_name)
     
-def extract_header(file_name):
-    data = fits.open(file_name)
+def extract_header(file_name,ext=1,verbose=False):
+    header = fits.open(file_name)[ext].header
+    if verbose:
+        print(header)
+    return header
