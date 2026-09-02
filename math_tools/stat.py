94dc8f04fe5409467d2b0cef3722a93e46664c3a
Frank Soldano <f.soldano38@orange.fr>
2026-09-02 11:32:42 +0200

    Add median_by_bins_2d()

--- math_tools/stat.py
+++ math_tools/stat.py
@@ -9,3 +9,24 @@ def Pearson_skewness(data):
   '''
   measure = 3*(np.mean(data) - np.median(data))/np.std(data)
   return measure
+
+def median_by_bins_2d(x,y,bins=10):
+  '''
+  Caluclate a median for y per x bins.
+  '''
+
+  hist_x = np.histogram(x,bins=bins)
+  hist_x_value = hist_x[1]
+
+  res = {'x_bin':[],'y_med':[]}
+
+  for i in range(len(hist_x_value)-1):
+    min_lim = hist_x_value[i]
+    max_lim = hist_x_value[i+1]
+
+    med = np.median(y[np.where((min_lim<=x) & (x<max_lim))])
+
+    res['x_bin'].append(np.mean([min_lim,max_lim]))
+    res['y_med'].append(med)
+
+  return res
