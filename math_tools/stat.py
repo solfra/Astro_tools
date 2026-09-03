4350a71bd0c1f1db60209c1ff64d62ac98b47a08
Frank Soldano <f.soldano38@orange.fr>
2026-09-03 10:31:11 +0200

    Code cleaning, add n_value

--- math_tools/stat.py
+++ math_tools/stat.py
@@ -10,23 +10,33 @@ def Pearson_skewness(data):
   measure = 3*(np.mean(data) - np.median(data))/np.std(data)
   return measure
 
-def median_by_bins_2d(x,y,bins=10):
+def median_by_bins(x,y,bins=10):
   '''
   Caluclate a median for y per x bins.
   '''
 
-  hist_x = np.histogram(x,bins=bins)
-  hist_x_value = hist_x[1]
+  _,hist_x_value = np.histogram(x,bins=bins)
 
-  res = {'x_bin':[],'y_med':[]}
+  res = {'x_bin':[],'y_med':[],'n_value':[]}
 
-  for i in range(len(hist_x_value)-1):
+  max_iter = len(hist_x_value)-1
+
+  for i in range(max_iter):
     min_lim = hist_x_value[i]
-    max_lim = hist_x_value[i+1]
+    max_lim = hist_x_value[i+1] 
 
-    med = np.median(y[np.where((min_lim<=x) & (x<max_lim))])
+    #increase max lim at last iter to include the max value
+    if i == max_iter - 1:
+      mask = (min_lim <= x) & (x <= max_lim)
+    else:
+      mask = (min_lim <= x) & (x < max_lim)
+
+    y_bin = y[mask]
+    med = np.median(y_bin)
+    n_val = y_bin.shape[0]
 
     res['x_bin'].append(np.mean([min_lim,max_lim]))
     res['y_med'].append(med)
+    res['n_value'].append(n_val)
 
   return res
